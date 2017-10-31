1a42ca29ecd29111889642f5b3fd5b648dfd4c69
Edvaldo Junior <dujunior1300@gmail.com>
2017-10-31 08:34:30 -0300

    feat(modes): create struct Player

---
+++ Ink Xsos/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Ink Xsos
+//
+//  Created by Edvaldo Junior on 31/10/17.
+//  Copyright © 2017 Edvaldo Junior. All rights reserved.
+//
+
+import Foundation
+
+struct Player<symbolType> {
+    
+    var symbol: symbolType
+    var number: Int
+}
