214f91151ab37689d47a35b925742c2072a41c85
Edvaldo Junior <dujunior1300@gmail.com>
2017-11-06 10:18:33 -0300

    feat: create class ButtonNode

---
+++ Ink Xsos/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  Ink Xsos
+//
+//  Created by Edvaldo Junior on 01/11/17.
+//  Copyright © 2017 Edvaldo Junior. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+}
