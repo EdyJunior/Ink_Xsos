d6c5a6d78b8565153f6e98f057fe24423293ef5c
Edvaldo Junior <dujunior1300@gmail.com>
2017-11-25 18:44:10 -0300

    feat: create protocol SoundController

--- Ink Xsos/Ink Xsos/GameViewController.swift
+++ Ink Xsos/Ink Xsos/GameViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import AVFoundation
+
+protocol SoundController {
+    
+    var sound: AVAudioPlayer? { get set }
+    
+    func stopSound()
+    func playSound()
+}
 
 class GameViewController: UIViewController {
 
+    var sound: AVAudioPlayer?
+    
     override func viewDidLoad() {
         
         let scene = MenuScene(size: view.bounds.size)
@@ -21,8 +32,22 @@ class GameViewController: UIViewController {
         skView.ignoresSiblingOrder = true
         scene.scaleMode = .resizeFill
         scene.backgroundColor = .white
+        scene.soundController = self
         
+        if defaultsStandard.soundOn() { playMenuSound() }
+
         skView.presentScene(scene)
+    }
+    
+    func playMenuSound() {
+        
+        let path = Bundle.main.path(forResource: "menuSound.wav", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        do {
+            sound = try AVAudioPlayer(contentsOf: url)
+            sound?.numberOfLoops = -1
+            sound?.play()
+        } catch { print("menuSound.wav doesn't exist!") }
     }
 
     override var shouldAutorotate: Bool {
@@ -44,4 +69,24 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+}
+
+extension GameViewController: SoundController {
+    
+    func stopSound() {
+
+        if let sound = sound {
+            sound.stop()
+            sound.currentTime = 0
+        }
+    }
+    
+    func playSound() {
+        
+        if let sound = sound {
+            if !sound.isPlaying {
+                playMenuSound()
+            }
+        } else { playMenuSound() }
+    }    
 }
