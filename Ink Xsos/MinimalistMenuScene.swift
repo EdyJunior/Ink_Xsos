3139b0b44139c9bff47d9fb560199b685e674156
Edvaldo Junior <dujunior1300@gmail.com>
2017-11-29 15:45:38 -0300

    feat(menu): create a copy of the MenuScene

---
+++ Ink Xsos/MinimalistMenuScene.swift
@@ -0,0 +1,13 @@
+//
+//  MinimalistMenuScene.swift
+//  Ink Xsos
+//
+//  Created by Edvaldo Junior on 27/11/17.
+//  Copyright © 2017 Edvaldo Junior. All rights reserved.
+//
+
+import UIKit
+
+class MinimalistMenuScene: SKScene {
+
+}
