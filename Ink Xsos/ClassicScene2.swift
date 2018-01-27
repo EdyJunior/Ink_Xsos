d4bc33f5ca10710378dce199d003d28338b81c62
Edvaldo Junior <dujunior1300@gmail.com>
2018-01-26 22:39:33 -0300

    refactor: use a ClassicGame in ClassicScene2

--- Ink Xsos/ClassicScene2.swift
+++ Ink Xsos/ClassicScene2.swift
@@ -8,10 +8,10 @@
 
 import SpriteKit
 
-class ClassicScene2: GameScene {
+class ClassicScene2: GameScene2 {
     
     var playerNumber = 1;
-    var classic = Classic()
+    var classic = ClassicGame()
     
     override func didMove(to view: SKView) {
         
@@ -23,118 +23,25 @@ class ClassicScene2: GameScene {
                 SKAction.playSoundFileNamed(Sounds.start, waitForCompletion: false)
             )
         }
-        buildScene()
-    }
-    
-    func buildScene() {
-        
-        buildCellButtons()
         resetGame()
     }
     
     func resetGame() {
         
-        for cb in cellButtons {
-            let gridFrame = self.grid.frame
-            
-            cb.pressed = false
-            cb.enabled = true
-            cb.size = CGSize(width: gridFrame.width / 3, height: gridFrame.height / 3)
-        }
-        for e in endGameSprites {
-            e.removeFromParent()
-        }
-        classic = Classic()
+        for e in endGameSprites { e.removeFromParent() }
+        endGameSprites.removeAll()
+        
+        classic = ClassicGame()
+        let player1 = ClassicPlayer(symbol: "X", number: 0, brush: .black)
+        let player2 = ClassicPlayer(symbol: "O", number: 1, brush: .black)
+        classic.players = [player1, player2]
+        grid.touchedProtocol = classic
+        
         playerNumber = 1
         modeLabel.text = "classic"
         messageLabel.text = "It’s X turn!"
-        timeLabel.text = "15"
-    }
-    
-    func changePlayerNumber() {
-        
-        if playerNumber == 1 { playerNumber = 2 }
-        else { playerNumber = 1 }
-        messageLabel.text = "It’s \(classic.getSymbol(fromPlayer: playerNumber)) turn!"
-    }
-    
-    func buildCellButton(inCell cell: [Int], inPos pos: CGPoint) {
-        
-        let button = Button(buttonAction: touchCell)
-        let gridFrame = self.grid.frame
-        button.size = CGSize(width: gridFrame.width / 3, height: gridFrame.height / 3)
-        button.position = pos
-        button.zPosition = 1
-        button.name = "\(cell[0]) \(cell[1])"
-        button.touchableArea.alpha = 0.01
-        
-        self.cellButtons.append(button)
-        self.addChild(button)
-    }
-    
-    func buildCellButtons() {
-        
-        for i in 1...3 {
-            for j in 1...3 {
-                let gridFrame = self.grid.frame
-                let gridPosition = self.grid.position
-                let gridLeft = gridPosition.x - gridFrame.width / 2
-                
-                let x = gridLeft
-                var xAux = CGFloat((2 * j - 1))
-                xAux *= grid.frame.width / 6
-                
-                var y = (grid.frame.midY - grid.frame.height / 2)
-                let yAux = CGFloat(2 * (4 - i) - 1)
-                y += (grid.frame.height *  yAux / 6.0)
-                
-                let point = CGPoint(x: x + xAux, y: y)
-                buildCellButton(inCell: [i, j], inPos: point)
-            }
-        }
-    }
-    
-    func touchCell(_ button: Button) {
-        
-        let btnName = button.name!
-        let pos = btnName.components(separatedBy: " ").flatMap { Int($0) }
-        
-        let success = classic.updateGrid(playerNumber: playerNumber, symb: (playerNumber == 1 ? "X" : "O"), pos: pos)
-        
-        if !success { print("Não é sua vez!") }
-        else {
-            button.touchableArea.alpha = 1.0
-            
-            let s = classic.getSymbol(fromPlayer: playerNumber)
-            let atlas = SKTextureAtlas(named: s)
-            let n = atlas.textureNames.count
-            let imageName = String.init(format: "\(s)_%0.3d", n)
-            button.activeButton.texture = SKTexture(imageNamed: imageName)
-            
-            let correctionFactor = CGFloat(0.6)
-            var newSize = button.size
-            newSize.width *= correctionFactor
-            newSize.height *= correctionFactor
-            button.size = newSize
-            
-            if defaultsStandard.animationsOn() {
-                button.animate(AtlasName: s)
-            }
-            button.enabled = false
-            
-            let state = classic.isGameOver()
-            if state == .finishedWithWinner {
-                let message = "\(s) wins"
-                messageLabel.text = message
-                endGame(victoryLine: classic.victoryLine!)
-            } else if state == .draw {
-                let message = "Draw"
-                messageLabel.text = message
-                addResetLabel()
-            } else if state == .onGoing {
-                changePlayerNumber()
-            }
-        }
+        grid.clear()
+        grid.lock(false)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -168,4 +75,3 @@ class ClassicScene2: GameScene {
         resetLabel.run(fadeAction)
     }
 }
-
