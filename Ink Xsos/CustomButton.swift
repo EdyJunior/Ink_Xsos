fe322af4d36762a3ce24d21d3620c50e203fa814
Edvaldo Junior <dujunior1300@gmail.com>
2018-02-12 22:27:13 -0300

    refactor(button): make a simpler custom button

---
+++ Ink Xsos/CustomButton.swift
@@ -0,0 +1,48 @@
+//
+//  CustomButton.swift
+//  Ink Xsos
+//
+//  Created by Edvaldo Junior on 12/02/2018.
+//  Copyright © 2018 Edvaldo Junior. All rights reserved.
+//
+
+import SpriteKit
+
+protocol OptionsDelegate {
+    func showOptions()
+}
+
+class CustomButton: SKNode {
+
+    var imageButton: SKSpriteNode
+    var touchableArea = SKSpriteNode()
+    var enabled: Bool = true
+    var pressed: Bool = false
+    var delegate: OptionsDelegate!
+ 
+    init(sprite: SKSpriteNode) {
+        
+        self.imageButton = sprite
+        self.touchableArea.size = sprite.size
+        super.init()
+        
+        self.touchableArea.addChild(imageButton)
+        self.addChild(touchableArea)
+        
+        isUserInteractionEnabled = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if touchableArea.contains((touches.first?.location(in: self))!) {
+            if enabled {
+                pressed = !pressed
+                delegate.showOptions()
+            }
+        }
+    }
+}
