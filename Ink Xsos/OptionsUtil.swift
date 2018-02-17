d4c69f55c94880e8d5d651a5e5cfcdd5f3fe5993
Edvaldo Junior <dujunior1300@gmail.com>
2018-02-16 22:32:42 -0300

    feat: add assets to options screen

---
+++ Ink Xsos/OptionsUtil.swift
@@ -0,0 +1,13 @@
+//
+//  OptionsUtil.swift
+//  Ink Xsos
+//
+//  Created by Edvaldo Junior on 15/02/2018.
+//  Copyright © 2018 Edvaldo Junior. All rights reserved.
+//
+
+import UIKit
+
+class OptionsUtil: NSObject {
+
+}
