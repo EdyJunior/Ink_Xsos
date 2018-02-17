07d5f93ebe9076f227429ee6f3af532f5109617f
Edvaldo Junior <dujunior1300@gmail.com>
2018-02-16 22:42:08 -0300

    feat(options): create a class that supports options screen in standard functions

--- Ink Xsos/OptionsUtil.swift
+++ Ink Xsos/OptionsUtil.swift
@@ -6,8 +6,56 @@
 //  Copyright © 2018 Edvaldo Junior. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
+
+enum TypeOfButton {
+    case back_to_menu, back_to_game, set_sound
+}
 
 class OptionsUtil: NSObject {
+    
+    var view: SKView?
+    var type: TypeOfButton
+    
+    init(type: TypeOfButton, view: SKView? = nil) {
+        
+        self.type = type
+        self.view = view
+        super.init()
+    }
+    
+    func backToMenu() {
+        
+        let sceneInstance = MenuScene(size: view!.bounds.size)
+        let transition = SKTransition.fade(with: .white, duration: 1.0)
+        view!.presentScene(sceneInstance, transition: transition)
+    }
+    
+    func backToGame() {
+        
+        let sceneInstance = MenuScene(size: view!.bounds.size)
+        let transition = SKTransition.fade(with: .white, duration: 1.0)
+        view!.presentScene(sceneInstance, transition: transition)
+    }
+    
+    func setSound() {
+        
+        let isSoundOn = defaultsStandard.soundOn()
+        defaultsStandard.set(!isSoundOn, forKey: Defaults.soundOn)
+    }
+}
 
+extension OptionsUtil: ButtonAction {
+    
+    func execute() {
+        
+        switch type {
+        case .back_to_game:
+            backToGame()
+        case .back_to_menu:
+            backToMenu()
+        default:
+            setSound()
+        }
+    }
 }
