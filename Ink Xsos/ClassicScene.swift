1ee429f2c604433d908353bf36ad1157f08e070b
Edvaldo Junior <dujunior1300@gmail.com>
2018-02-17 21:54:50 -0300

    refactor(options): create OptionsAction to remove ButtonAction from ClassicScene

--- Ink Xsos/ClassicScene.swift
+++ Ink Xsos/ClassicScene.swift
@@ -40,9 +40,10 @@ class ClassicScene: GameScene {
         }
         resetGame()
         
+        let optionsAction = OptionsAction(scene: scene!, inGame: true)
         let optionsButton = OptionsButton(sceneSize: self.scene!.size)
         optionsButton.position = CGPoint(x: scene!.size.width * 0.13, y: scene!.size.height * 0.93)
-        optionsButton.action = self
+        optionsButton.action = optionsAction
         addChild(optionsButton)
     }
     
@@ -156,98 +157,5 @@ extension ClassicScene: MatchPresentationManager {
         
         messageLabel.text = "It's \(classic.currentPlayer.symbol) turn!"
         grid.lock(false)
-    }
-}
-
-extension ClassicScene: ButtonAction {
-    
-    func execute() {
-        
-        let sceneFrame = scene!.frame
-        
-        let blank = SKSpriteNode(texture: SKTexture(imageNamed: "blank"), color: .clear, size: scene!.size)
-        blank.position = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height / 2)
-        blank.zPosition = 10
-        blank.alpha = 0.9
-        
-        let splashTexture = SKTexture(imageNamed: Images.Spots.option_splash)
-        let splashProportion = splashTexture.size().height / splashTexture.size().width
-        let splashSize = CGSize(width: sceneFrame.width, height: sceneFrame.width * splashProportion)
-        let splash = SKSpriteNode(texture: splashTexture, color: .clear, size: splashSize)
-        splash.position = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height - splashSize.height / 2)
-        splash.zPosition = blank.zPosition + 1
-        
-        let device = UIDevice.current.userInterfaceIdiom
-        let factor: CGFloat = device == .phone ? 0.18 : 0.14
-        
-        let optionsLabel = SKLabelNode(text: "options")
-        optionsLabel.fontSize = sceneFrame.width * factor
-        optionsLabel.position = CGPoint(x: sceneFrame.midX, y: size.height * 0.915)
-        optionsLabel.fontColor = .white
-        optionsLabel.zPosition = splash.zPosition + 1
-        optionsLabel.fontName = Fonts.ink
-        
-        let soundButtonTexture = SKTexture(imageNamed: "\(Images.Buttons.sound)_001")
-        let soundButtonProportion = soundButtonTexture.size().height / soundButtonTexture.size().width
-        let soundButtonWidth = sceneFrame.width * 0.55
-        let soundButtonSize = CGSize(width: soundButtonWidth, height: soundButtonWidth * soundButtonProportion)
-        let soundButtonSprite = SKSpriteNode(texture: soundButtonTexture, color: .clear, size: soundButtonSize)
-        let soundButton = CustomButton(sprite: soundButtonSprite)
-        soundButton.position = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height * 0.65)
-        soundButton.zPosition = blank.zPosition + 1
-        let optionSound = OptionsUtil(type: .set_sound, view: self.view!)
-        soundButton.action = optionSound
-        
-        if defaultsStandard.bool(forKey: Defaults.soundOn) {
-            let number = soundButtonSprite.numberOfFrames(forImageNamed: Images.Buttons.sound)
-            let name = String.init(format: "\(Images.Buttons.sound)_%03d", number)
-            soundButton.imageButton.texture = SKTexture(imageNamed: name)
-        }
-        soundButton.action = optionSound
-        
-        let back1ButtonTexture = SKTexture(imageNamed: Images.Buttons.backGame)
-        let back1ButtonProportion = back1ButtonTexture.size().height / back1ButtonTexture.size().width
-        let back1ButtonWidth = sceneFrame.width * 0.75
-        let back1ButtonSize = CGSize(width: back1ButtonWidth, height: back1ButtonWidth * back1ButtonProportion)
-        let back1ButtonSprite = SKSpriteNode(texture: back1ButtonTexture, color: .clear, size: back1ButtonSize)
-        let back1Button = CustomButton(sprite: back1ButtonSprite)
-        back1Button.position = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height * 0.4)
-        let optionBack1 = OptionsUtil(type: .back_to_game, view: self.view!)
-        back1Button.action = optionBack1
-        back1Button.zPosition = blank.zPosition + 1
-        
-        let back1Label = SKLabelNode(text: "back to game")
-        back1Label.fontSize = sceneFrame.width * factor * 0.45
-        back1Label.position = CGPoint(x: 0, y: back1ButtonSize.height * -0.05)
-        back1Label.fontColor = .white
-        back1Label.zPosition = back1Button.zPosition + 1
-        back1Label.fontName = Fonts.ink
-        back1Button.touchableArea.addChild(back1Label)
-        
-        let back2ButtonTexture = SKTexture(imageNamed: Images.Buttons.backMenu)
-        let back2ButtonProportion = back2ButtonTexture.size().height / back2ButtonTexture.size().width
-        let back2ButtonWidth = sceneFrame.width * 0.75
-        let back2ButtonSize = CGSize(width: back2ButtonWidth, height: back2ButtonWidth * back2ButtonProportion)
-        let back2ButtonSprite = SKSpriteNode(texture: back2ButtonTexture, color: .clear, size: back2ButtonSize)
-        let back2Button = CustomButton(sprite: back2ButtonSprite)
-        back2Button.position = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height * 0.15)
-        let optionBack2 = OptionsUtil(type: .back_to_menu, view: self.view!)
-        back2Button.action = optionBack2
-        back2Button.zPosition = blank.zPosition + 1
-        
-        let back2Label = SKLabelNode(text: "back to menu")
-        back2Label.fontSize = sceneFrame.width * factor * 0.45
-        back2Label.position = CGPoint(x: 0, y: back2ButtonSize.height * -0.1)
-        back2Label.fontColor = .white
-        back2Label.zPosition = back2Button.zPosition + 1
-        back2Label.fontName = Fonts.ink
-        back2Button.touchableArea.addChild(back2Label)
-        
-        addChild(blank)
-        addChild(splash)
-        addChild(optionsLabel)
-        addChild(soundButton)
-        addChild(back1Button)
-        addChild(back2Button)
     }
 }
---
+++ Ink Xsos/OptionsAction.swift
@@ -0,0 +1,133 @@
+//
+//  OptionsAction.swift
+//  Ink Xsos
+//
+//  Created by Edvaldo Junior on 17/02/2018.
+//  Copyright © 2018 Edvaldo Junior. All rights reserved.
+//
+
+import SpriteKit
+
+class OptionsAction: NSObject {
+
+    var scene: SKScene
+    var uiElements = [SKNode]()
+    let blankZPos: CGFloat = 10
+    var inGame: Bool
+    
+    var sceneFrame: CGRect { return scene.frame }
+
+    init(scene: SKScene, inGame: Bool) {
+        
+        self.scene = scene
+        self.inGame = inGame
+    }
+    
+    func setBackground() {
+
+        let blank = SKSpriteNode(texture: SKTexture(imageNamed: "blank"), color: .clear, size: scene.size)
+        blank.position = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height / 2)
+        blank.zPosition = blankZPos
+        blank.alpha = 0.9
+        
+        let splashTexture = SKTexture(imageNamed: Images.Spots.option_splash)
+        let splashProportion = splashTexture.size().height / splashTexture.size().width
+        let splashSize = CGSize(width: sceneFrame.width, height: sceneFrame.width * splashProportion)
+        let splash = SKSpriteNode(texture: splashTexture, color: .clear, size: splashSize)
+        splash.position = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height - splashSize.height / 2)
+        splash.zPosition = blank.zPosition + 1
+        
+        let device = UIDevice.current.userInterfaceIdiom
+        let factor: CGFloat = device == .phone ? 0.18 : 0.14
+        
+        let optionsLabel = SKLabelNode(text: "options")
+        optionsLabel.fontSize = sceneFrame.width * factor
+        optionsLabel.position = CGPoint(x: sceneFrame.midX, y: sceneFrame.height * 0.915)
+        optionsLabel.fontColor = .white
+        optionsLabel.zPosition = splash.zPosition + 1
+        optionsLabel.fontName = Fonts.ink
+        
+        scene.addChild(blank)
+        scene.addChild(splash)
+        scene.addChild(optionsLabel)
+        
+        uiElements.append(blank)
+        uiElements.append(splash)
+        uiElements.append(optionsLabel)
+    }
+    
+    func setSoundButton() {
+        
+        let soundButtonTexture = SKTexture(imageNamed: "\(Images.Buttons.sound)_001")
+        let soundButtonProportion = soundButtonTexture.size().height / soundButtonTexture.size().width
+        let soundButtonWidth = sceneFrame.width * 0.55
+        let soundButtonSize = CGSize(width: soundButtonWidth, height: soundButtonWidth * soundButtonProportion)
+        let soundButtonSprite = SKSpriteNode(texture: soundButtonTexture, color: .clear, size: soundButtonSize)
+        let soundButton = CustomButton(sprite: soundButtonSprite)
+        soundButton.position = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height * 0.65)
+        soundButton.zPosition = blankZPos + 1
+        let optionSound = OptionsUtil(type: .set_sound, view: scene.view!)
+        soundButton.action = optionSound
+        
+        if defaultsStandard.bool(forKey: Defaults.soundOn) {
+            let number = soundButtonSprite.numberOfFrames(forImageNamed: Images.Buttons.sound)
+            let name = String.init(format: "\(Images.Buttons.sound)_%03d", number)
+            soundButton.imageButton.texture = SKTexture(imageNamed: name)
+        }
+        soundButton.action = optionSound
+        
+        scene.addChild(soundButton)
+        uiElements.append(soundButton)
+    }
+    
+    func setBackButton(imageName: String, pos: CGPoint, text: String, type: TypeOfButton) {
+        
+        let backButtonTexture = SKTexture(imageNamed: imageName)
+        let backButtonProportion = backButtonTexture.size().height / backButtonTexture.size().width
+        let backButtonWidth = sceneFrame.width * 0.75
+        let backButtonSize = CGSize(width: backButtonWidth, height: backButtonWidth * backButtonProportion)
+        let backButtonSprite = SKSpriteNode(texture: backButtonTexture, color: .clear, size: backButtonSize)
+        let backButton = CustomButton(sprite: backButtonSprite)
+        backButton.position = pos
+        let optionBack = OptionsUtil(type: type, view: scene.view!)
+        backButton.action = optionBack
+        backButton.zPosition = blankZPos + 1
+        
+        let device = UIDevice.current.userInterfaceIdiom
+        let factor: CGFloat = device == .phone ? 0.18 : 0.14
+        
+        let backLabel = SKLabelNode(text: text)
+        backLabel.fontSize = sceneFrame.width * factor * 0.45
+        backLabel.position = CGPoint(x: 0, y: backButtonSize.height * -0.05)
+        backLabel.fontColor = .white
+        backLabel.zPosition = backButton.zPosition + 1
+        backLabel.fontName = Fonts.ink
+        backButton.touchableArea.addChild(backLabel)
+        
+        scene.addChild(backButton)
+        uiElements.append(backButton)
+    }
+}
+
+extension OptionsAction: ButtonAction {
+    
+    func execute() {
+        
+        let backToGamePos = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height * 0.4)
+        let backToMenuInGamePos = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height * 0.15)
+        let backToMenuOutGamePos = CGPoint(x: sceneFrame.width / 2, y: sceneFrame.height * 0.35)
+        
+        setBackground()
+        setSoundButton()
+        if inGame {
+            setBackButton(imageName: Images.Buttons.backGame,
+                          pos: backToGamePos,
+                          text: "back to game",
+                          type: .back_to_game)
+        }
+        setBackButton(imageName: Images.Buttons.backMenu,
+                      pos: inGame ? backToMenuInGamePos : backToMenuOutGamePos,
+                      text: "back to menu",
+                      type: .back_to_menu)
+    }
+}
