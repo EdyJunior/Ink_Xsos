69442dc88f883241a72b0269942a0a07a2e03233
Edvaldo Junior <dujunior1300@gmail.com>
2018-02-23 23:02:51 -0300

    feat (design): add a new animated background to GameScene

--- Ink Xsos/GameScene.swift
+++ Ink Xsos/GameScene.swift
@@ -34,7 +34,42 @@ class GameScene: SKScene {
     
     private func buildBackground() {
         
+        let gridZPos = grid.zPosition
+        let w = scene!.frame.width
+        let h = scene!.frame.height
+        let texture1 = SKTexture(imageNamed: "splash_001")
+        let texture2 = SKTexture(imageNamed: "splash_002")
+        let texture3 = SKTexture(imageNamed: "splash_006")
+        let position1 = CGPoint(x: w * 1.15, y: h)
+        let position2 = CGPoint(x: -w * 0.2, y: h * 0.4)
+        let position3 = CGPoint(x: w * 1.2, y: h * 0.2)
+        let proportion = texture1.size().height / texture1.size().width
+        let splashesSize = CGSize(width: w, height: w * proportion)
         
+        let splash1 = SKSpriteNode(texture: texture1, color: .clear, size: splashesSize)
+        let splash2 = SKSpriteNode(texture: texture2, color: .clear, size: splashesSize)
+        let splash3 = SKSpriteNode(texture: texture3, color: .clear, size: splashesSize)
+        splash1.position = position1
+        splash2.position = position2
+        splash3.position = position3
+        splash1.zPosition = gridZPos - 1
+        splash2.zPosition = gridZPos - 1
+        splash3.zPosition = gridZPos - 1
+        splash1.alpha = 0
+        splash2.alpha = 0
+        splash3.alpha = 0
+        
+        addChild(splash1)
+        addChild(splash2)
+        addChild(splash3)
+        
+        let wait = SKAction.wait(forDuration: 0.3)
+        let fade = SKAction.fadeAlpha(to: 1.0, duration: 0.3)
+        let act1 = SKAction.run { splash1.run(fade) }
+        let act2 = SKAction.run { splash2.run(fade) }
+        let act3 = SKAction.run { splash3.run(fade) }
+        let act = SKAction.sequence([act1, wait, act2, wait, act3])
+        scene?.run(act)
     }
     
     private func buildModeLabel() {
